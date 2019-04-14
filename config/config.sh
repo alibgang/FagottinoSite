5658ec4e2b6a9994e6fb572128119ea1fbdf5737
Leo Reading <leo.reading@gmail.com>
2019-04-14 14:33:29 -0400

    Added config for nginx

--- config/config.sh
+++ config/config.sh
@@ -5,6 +5,7 @@
 
 # Copy the configs we want to use
 cp "./nginx/$1.env" ./nginx/nginx.env
+cp "./nginx/$1.conf" ./nginx/nginx.conf
 
 # Load the env vars to the .env file that lives at the root of the project
 cat ./nginx/nginx.env >> ../.env
